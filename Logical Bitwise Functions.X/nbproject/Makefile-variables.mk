2b377a8916e68bef74473a880cca577751d75f7b
znic1967-UB <zackary.k.nicholson@gmail.com>
2017-10-03 09:37:52 -0700

    Added programming assignment 2

---
+++ Logical Bitwise Functions.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Logical_Bitwise_Functions.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Logical_Bitwise_Functions.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=logicalbitwisefunctions.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/logicalbitwisefunctions.x.tar
